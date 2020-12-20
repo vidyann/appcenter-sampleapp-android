a1f79fa622308b4d384bc18d794b1706eea43dbe
vidyann <2vidya.narasimhan@gmail.com>
2020-12-20 22:31:24 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/ms/appcenter/sampleapp/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ms/appcenter/sampleapp/android/ExampleInstrumentedTest.java
@@ -4,6 +4,14 @@ import android.content.Context;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.accessibility.AccessibilityChecks;
+import androidx.test.filters.LargeTest;
+
 
 import org.junit.Rule;
 import org.junit.After;
